a70cceed9c27f8c0f515e409d8f931fbc42d43d3
hanifan <hanifannurulhaq@outlook.com>
2023-07-23 09:17:23 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.siakad_lpk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:siakad_lpk/app/app.dart';
+import 'package:siakad_lpk/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
