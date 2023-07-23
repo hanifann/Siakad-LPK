e08b92424857d432c65ab9117831ccf74e39aa2c
hanifan <hanifannurulhaq@outlook.com>
2023-07-23 10:47:40 +0700

    add login and register routers

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,12 +1,22 @@
 import 'package:go_router/go_router.dart';
+import 'package:siakad_lpk/features/landing/presentation/views/landing_view.dart';
 import 'package:siakad_lpk/features/login/presentation/views/login_view.dart';
+import 'package:siakad_lpk/features/register/presentation/views/register_view.dart';
 
 // GoRouter configuration
 final router = GoRouter(
   routes: [
     GoRoute(
       path: '/',
+      builder: (context, state) => const LandingView(),
+    ),
+    GoRoute(
+      path: '/login',
       builder: (context, state) => const LoginView(),
+    ),
+    GoRoute(
+      path: '/register',
+      builder: (context, state) => const RegisterView(),
     ),
   ],
 );
