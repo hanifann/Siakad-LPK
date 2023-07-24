953ac71a34cd12176b75074364566c0f2825358b
hanifan <hanifannurulhaq@outlook.com>
2023-07-24 14:09:32 +0700

    add profile route

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -2,6 +2,7 @@ import 'package:go_router/go_router.dart';
 import 'package:siakad_lpk/features/auth/presentation/views/auth_view.dart';
 import 'package:siakad_lpk/features/landing/presentation/views/landing_view.dart';
 import 'package:siakad_lpk/features/login/presentation/views/login_view.dart';
+import 'package:siakad_lpk/features/profile/presentation/views/profile_view.dart';
 import 'package:siakad_lpk/features/register/presentation/views/register_view.dart';
 
 // GoRouter configuration
@@ -22,6 +23,10 @@ final router = GoRouter(
     GoRoute(
       path: '/register',
       builder: (context, state) => const RegisterView(),
+    ),
+    GoRoute(
+      path: '/profile',
+      builder: (context, state) => const ProfileView(),
     ),
   ],
 );
