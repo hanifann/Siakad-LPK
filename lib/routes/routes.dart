4839139eaaada39c464c41f0a2ab066e7a18532d
hanifan <hanifannurulhaq@outlook.com>
2023-07-24 08:39:43 +0700

    change root route, add landing route

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:siakad_lpk/features/auth/presentation/views/auth_view.dart';
 import 'package:siakad_lpk/features/landing/presentation/views/landing_view.dart';
 import 'package:siakad_lpk/features/login/presentation/views/login_view.dart';
 import 'package:siakad_lpk/features/register/presentation/views/register_view.dart';
@@ -8,6 +9,10 @@ final router = GoRouter(
   routes: [
     GoRoute(
       path: '/',
+      builder: (context, state) => const AuthView(),
+    ),
+    GoRoute(
+      path: '/landing',
       builder: (context, state) => const LandingView(),
     ),
     GoRoute(
