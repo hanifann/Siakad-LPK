707075c6b7bb44e4fac78c1265ffe00003e823e4
hanifan <hanifannurulhaq@outlook.com>
2023-07-24 08:19:36 +0700

    initialize dependency injection

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,11 @@
-import 'package:siakad_lpk/app/app.dart';
-import 'package:siakad_lpk/bootstrap.dart';
 
-void main() {
+import 'package:flutter/widgets.dart';
+import 'app/view/app.dart';
+import 'bootstrap.dart';
+import 'injection_container.dart' as di;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
   bootstrap(() => const App());
 }
