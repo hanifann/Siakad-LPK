ca7de33ef0fda7de49176552d0b1921718d7dac8
hanifan <hanifannurulhaq@outlook.com>
2023-07-24 16:29:49 +0700

    add secondary color

--- lib/themes/colors.dart
+++ lib/themes/colors.dart
@@ -2,5 +2,6 @@ import 'package:flutter/material.dart';
 
 const kPrimaryTextColor = Color.fromRGBO(18, 19, 22, 1);
 const kPrimaryColor = Color.fromRGBO(69, 36, 37, 1);
+const kSecondaryColor = Color.fromRGBO(139, 109, 108, 1);
 const kErrorColor = Color.fromRGBO(200, 0, 3, 1);
 const kSecondaryTextColor = Color.fromRGBO(111, 112, 114, 1);
