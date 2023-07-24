458d8d22214841e82f1d30fb51118123a64a4005
hanifan <hanifannurulhaq@outlook.com>
2023-07-24 14:09:24 +0700

    update request to multipart request

--- lib/features/register/data/datasources/register_remote_datasources.dart
+++ lib/features/register/data/datasources/register_remote_datasources.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:developer';
 
 import 'package:http/http.dart';
 import 'package:siakad_lpk/core/error/exception.dart';
@@ -17,17 +18,39 @@ class RegisterRemoteDataSourceImpl implements RegisterRemoteDataSource {
 
   @override
   Future<String> postRegister(RegisterModel register) async {
-    final response = await client.post(
-      Uri.http(Env.url, '/api/pendaftaran'),
-      headers: {
-        'Accept': 'application/json'
-      },
-      body: register.toJson()
-    );
+    final request = MultipartRequest('POST', Uri.http(Env.url, '/api/pendaftaran'));
+    request.fields['full_name'] = register.fullName;
+    request.fields['email'] = register.email;
+    request.fields['password'] = register.password;
+    request.fields['ktp'] = register.ktp;
+    request.fields['kontak'] = register.kontak;
+    request.fields['kelas'] = register.kelas;
+    request.fields['alamat'] = register.alamat;
+    request.fields['tempat_lahir'] = register.tempatLahir;
+    request.fields['tgl_lahir'] = register.tglLahir;
+    request.fields['pendidikan_terakhir'] = register.pendidikanTerakhir;
+    request.fields['status'] = register.status;
+    request.fields['agama'] = register.agama;
+    request.fields['nama_ortu'] = register.namaOrtu;
+    request.fields['pengalaman_kerja'] = register.pengalaman;
+    request.fields['metode_bayar'] = register.metodeBayar;
+    
+    final profilePic = await MultipartFile.fromPath('image', register.image);
+    final proof = await MultipartFile.fromPath('bukti_bayar', register.image);
+
+    request.files.add(profilePic);
+    request.files.add(proof);
+
+    request.headers['Accept'] = 'application/json';
+
+    final response = await request.send();
+    var responseData = await response.stream.toBytes();
+   var responseString = String.fromCharCodes(responseData);
+   log(responseString);
     if(response.statusCode == 200){
       return 'Success';
     } else {
-      throw ServerException(ErrorModel.fromJson(jsonDecode(response.body)));
+      throw ServerException(const ErrorModel(message: 'Telah terjadi kesalahan, silahkan coba lagi'));
     }
   }
   
