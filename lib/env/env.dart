762990346dee7ae9d36b87bc6a12bac606634f34
hanifan <hanifannurulhaq@outlook.com>
2023-07-24 16:30:16 +0700

    add key for info and jadwal

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -10,4 +10,8 @@ abstract class Env {
   static String token = _Env.token;
   @EnviedField(obfuscate: true, varName: 'USER_KEY')
   static String user = _Env.user;
+  @EnviedField(obfuscate: true, varName: 'INFO_KEY')
+  static String info = _Env.info;
+  @EnviedField(obfuscate: true, varName: 'JADWAL_KEY')
+  static String jadwal = _Env.jadwal;
 }
