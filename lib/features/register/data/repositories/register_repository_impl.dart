0e28b6d960ff055d8320f710139370a4f7a45805
hanifan <hanifannurulhaq@outlook.com>
2023-07-24 07:23:29 +0700

    remove unused import

--- lib/features/register/data/repositories/register_repository_impl.dart
+++ lib/features/register/data/repositories/register_repository_impl.dart
@@ -4,7 +4,6 @@ import 'package:siakad_lpk/core/error/failure.dart';
 import 'package:siakad_lpk/core/platform/network_info.dart';
 import 'package:siakad_lpk/features/register/data/datasources/register_remote_datasources.dart';
 import 'package:siakad_lpk/features/register/data/models/register_model.dart';
-import 'package:siakad_lpk/features/register/domain/entities/register.dart';
 import 'package:siakad_lpk/features/register/domain/repositories/register_repository.dart';
 
 class RegisterRepositoryImpl implements RegisterRepository {
