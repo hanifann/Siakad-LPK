c9db4a57069f552c7aa5d208e7ad5a0db604c20a
hanifann <hanifannurulhaq@outlook.com>
2023-07-26 13:50:02 +0700

    add hint as param

--- lib/features/register/presentation/widgets/custom_dropdown_btn_widget.dart
+++ lib/features/register/presentation/widgets/custom_dropdown_btn_widget.dart
@@ -6,16 +6,19 @@ class CustomDropdownBtnWidget extends StatelessWidget {
   const CustomDropdownBtnWidget({
     super.key, 
     required this.items, 
-    required this.onChange
+    required this.onChange,
+    this.hint = ''
   });
   final List<DropdownMenuItem<String>> items;
   final Function(String?) onChange;
+  final String hint;
 
   @override
   Widget build(BuildContext context) {
     return DropdownButtonFormField<String>(
       items: items,
       onChanged: onChange,
+      hint: Text(hint),
       decoration: InputDecoration(
         contentPadding: EdgeInsets.symmetric(horizontal: 12.w, vertical: 16.h),
         border: InputBorder.none,
